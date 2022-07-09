53caa0b5e2bd9f3a9469866ad1975f91d9f57444
André Luís da Silva <andre.silva@aluno.unievangelica.edu.br>
2022-07-09 00:20:06 -0300

    feat: Toggle btn icon from value notifier listener

--- presenter/lib/ui/home/screens/main_page.dart
+++ presenter/lib/ui/home/screens/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:presenter/ui/home/stores/movie_controller.dart';
+import 'package:presenter/ui/home/stores/stores.dart';
 import '../../../dependencies/dependencies.dart';
 import '../components/custom_list_card_widget.dart';
 import 'package:features/features.dart';
@@ -12,7 +13,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
- 
   late final MovieController _controller;
 
   @override
@@ -20,45 +20,48 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     _controller = Modular.get<MovieController>();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Movies',style:TextStyle(color: Colors.white),),
-        actions: [
-        const Align(
-          alignment: Alignment.center,
-          child: Text('0'),
+        appBar: AppBar(
+          title: const Text(
+            'Movies',
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: [
+            const Align(
+              alignment: Alignment.center,
+              child: Text('0'),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.favorite),
+              color: Colors.red,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(const CircleBorder()),
+                    backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                      (Set<MaterialState> states) {
+                        if (states.contains(MaterialState.pressed)) {
+                          return Colors.black;
+                        }
+
+                        return const Color.fromARGB(
+                            144, 158, 158, 158); // Use the component's default.
+                      },
+                    ),
+                  ),
+                  child: const Icon(Icons.person)),
+            ),
+          ],
+          elevation: 1,
+          backgroundColor: const Color.fromARGB(59, 0, 0, 0),
         ),
-        IconButton(
-          onPressed: (){},
-          icon: Icon(Icons.favorite),
-          color: Colors.red,
-         ),
-         Padding(
-           padding: const EdgeInsets.fromLTRB(10,10,10,10),
-           child: ElevatedButton(
-            onPressed: (){},
-             style:ButtonStyle(
-               shape:  MaterialStateProperty.all(CircleBorder()),
-               backgroundColor: MaterialStateProperty.resolveWith<Color>(
-              (Set<MaterialState> states) {
-                if (states.contains(MaterialState.pressed))
-                return Colors.black;
-                  return Color.fromARGB(144, 158, 158, 158); // Use the component's default.
-                },
-              ), 
-           ),
-            child: Icon(Icons.person)
-           ),
-           
-         ),
-         
-        ],
-        elevation: 1,
-        backgroundColor: Color.fromARGB(59, 0, 0, 0), 
-      ),
-       
         body: Padding(
             padding: const EdgeInsets.all(28),
             child: SingleChildScrollView(
@@ -66,43 +69,47 @@ class _HomePageState extends State<HomePage> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(10,5,10,5),
+                    padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
                     child: Container(
                       decoration: BoxDecoration(
-                        color: Color.fromARGB(34, 255, 255, 255),
+                        color: const Color.fromARGB(34, 255, 255, 255),
                         borderRadius: BorderRadius.circular(30),
                       ),
                       child: TextField(
                         onChanged: _controller.onChanged,
                         decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        prefixIcon: Icon(Icons.search),
-                            fillColor: Colors.white30,
-                            focusColor: Colors.white30,
-                            hoverColor: Colors.white30,
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.search),
+                          fillColor: Colors.white30,
+                          focusColor: Colors.white30,
+                          hoverColor: Colors.white30,
                         ),
-                          cursorColor: Colors.white30,
+                        cursorColor: Colors.white30,
                       ),
                     ),
                   ),
                   const SizedBox(
                     height: 35,
                   ),
-
-                      ValueListenableBuilder<MoviesEntity?>(
-                        valueListenable: _controller.movies,
-                         builder: (_,movies,__) {
-                            return movies != null
+                  ValueListenableBuilder<MoviesEntity?>(
+                      valueListenable: _controller.movies,
+                      builder: (_, movies, __) {
+                        return movies != null
                             ? ListView.separated(
                                 physics: const NeverScrollableScrollPhysics(),
                                 shrinkWrap: true,
                                 itemCount: movies.listMovies.length,
                                 itemBuilder: (__, idx) => CustomListCardWidget(
-                                    movie: MovieDetailDto.fromEntity(movies.listMovies[idx])),
+                                  movie: MovieDetailDto.fromEntity(
+                                    movies.listMovies[idx]
+                                  ),
+                                  controller: FavoriteController(),
+                                ),
                                 separatorBuilder: (__, _) => const Divider())
-                            : const Center(child: CircularProgressIndicator(),);
-                         }
-                      )
+                            : const Center(
+                                child: CircularProgressIndicator(),
+                              );
+                      })
                 ],
               ),
             )));
